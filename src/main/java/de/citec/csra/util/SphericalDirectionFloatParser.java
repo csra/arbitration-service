d3eb1a813a201600a821d3db703fcd9d0c1bf1d6
Patrick Holthaus <patrick.holthaus@uni-bielefeld.de>
2016-01-11 16:38:05 +0100

    add spherical direction parser

---
+++ src/main/java/de/citec/csra/util/SphericalDirectionFloatParser.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.citec.csra.util;
+
+import rst.geometry.SphericalDirectionFloatType.SphericalDirectionFloat;
+
+/**
+ *
+ * @author Patrick Holthaus
+ * (<a href=mailto:patrick.holthaus@uni-bielefeld.de>patrick.holthaus@uni-bielefeld.de</a>)
+ */
+public class SphericalDirectionFloatParser implements StringParser<SphericalDirectionFloat>{
+
+	@Override
+	public SphericalDirectionFloat getValue(String val) throws IllegalArgumentException {
+		String[] pt= val.split(",");
+		if(pt.length != 2){
+			throw new IllegalArgumentException("Illegal angle: " + val);
+		}
+		
+		SphericalDirectionFloat angle = SphericalDirectionFloat.newBuilder().
+				setElevation(Float.valueOf(pt[0])).
+				setAzimuth(Float.valueOf(pt[1])).build();
+		
+		return angle;
+	}
+	
+}
