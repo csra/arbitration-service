62d4ebc0bcea3e0c73b92f00c247dfdcc8fbe637
Patrick Holthaus <patrick.holthaus@uni-bielefeld.de>
2016-05-19 08:10:27 +0200

    type in rst-csra 0.12 does not have duration field

--- src/main/java/de/citec/csra/arbitration/srv/DefaultAllocationMap.java
+++ src/main/java/de/citec/csra/arbitration/srv/DefaultAllocationMap.java
@@ -7,10 +7,7 @@ package de.citec.csra.arbitration.srv;
 
 import rsb.Event;
 import rst.communicationpatterns.ResourceAllocationType.ResourceAllocation;
-import static rst.communicationpatterns.ResourceAllocationType.ResourceAllocation.Initiator.SYSTEM;
-import static rst.communicationpatterns.ResourceAllocationType.ResourceAllocation.Priority.NORMAL;
 import rst.communicationpatterns.TaskStateType.TaskState;
-import rst.timing.DurationType;
 
 /**
  *
@@ -18,23 +15,28 @@ import rst.timing.DurationType;
  * (<a href=mailto:patrick.holthaus@uni-bielefeld.de>patrick.holthaus@uni-bielefeld.de</a>)
  */
 public class DefaultAllocationMap {
-	private DefaultAllocationMap() {}
+
+	private DefaultAllocationMap() {
+	}
 	private static DefaultAllocationMap instance;
-	public static DefaultAllocationMap getInstance(){
-		if(instance == null){
+
+	public static DefaultAllocationMap getInstance() {
+		if (instance == null) {
 			instance = new DefaultAllocationMap();
 		}
 		return instance;
 	}
-	
+
 	public ResourceAllocation getRequiredResources(TaskState ts, Event e) {
-		ResourceAllocation a = ResourceAllocation.newBuilder().
-				setDescription("example").
-				setDuration(DurationType.Duration.newBuilder().setTime(1000)).
-				setInitiator(SYSTEM).
-				setPriority(NORMAL).
-				addLocationIds("Kitchen").
-				build();
-		return a;
+//		TODO: This is a dummy method
+//		ResourceAllocation a = ResourceAllocation.newBuilder().build()
+//				setDescription("example").
+//				setDuration(DurationType.Duration.newBuilder().setTime(1000)).
+//				setInitiator(SYSTEM).
+//				setPriority(NORMAL).
+//				addLocationIds("Kitchen").
+//				build();
+//		return a;
+		return null;
 	}
 }
