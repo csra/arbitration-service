342fd2713b1fc1bdaeb85a022cc274e0b2440aeb
Patrick Holthaus <patrick.holthaus@uni-bielefeld.de>
2016-06-20 16:58:47 +0200

    use isalive instead of contains for existence check

--- src/main/java/de/citec/csra/allocation/srv/Allocations.java
+++ src/main/java/de/citec/csra/allocation/srv/Allocations.java
@@ -40,110 +40,131 @@ public class Allocations {
 	}
 
 	public Map<String, ResourceAllocation> getMap() {
-		return new HashMap<>(this.allocations);
+		synchronized (this.allocations) {
+			return new HashMap<>(this.allocations);
+		}
 	}
 
-	synchronized boolean isAlive(String id) {
-		if (this.allocations.containsKey(id)) {
-			ResourceAllocation a = this.allocations.get(id);
-			State s = a.getState();
-			switch (s) {
-				case REJECTED:
-				case CANCELLED:
-				case ABORTED:
-				case RELEASED:
-					return false;
-				case ALLOCATED:
-				case REQUESTED:
-				case SCHEDULED:
-				default:
-					return true;
+	boolean isAlive(String id) {
+		synchronized (this.allocations) {
+			if (this.allocations.containsKey(id)) {
+				ResourceAllocation a = this.allocations.get(id);
+				State s = a.getState();
+				switch (s) {
+					case REJECTED:
+					case CANCELLED:
+					case ABORTED:
+					case RELEASED:
+						return false;
+					case ALLOCATED:
+					case REQUESTED:
+					case SCHEDULED:
+					default:
+						return true;
+				}
+			} else {
+				LOG.log(Level.WARNING, "attempt to check alive state for allocation ''{0}'' ignored, no such allocation available", id);
 			}
-		} else {
-			LOG.log(Level.WARNING, "attempt to check alive state for allocation ''{0}'' ignored, no such allocation available", id);
-		}
-		return false;
-	}
-
-	synchronized ResourceAllocation get(String id) {
-		if (this.allocations.containsKey(id)) {
-			ResourceAllocation a = this.allocations.get(id);
-			return a;
-		} else {
-			LOG.log(Level.WARNING, "attempt to query for allocation ''{0}'' ignored, no such allocation available", id);
-		}
-		return null;
-	}
-
-	synchronized void setState(String id, State newState) {
-		if (this.allocations.containsKey(id)) {
-			this.allocations.put(id,
-					ResourceAllocation.
-					newBuilder(this.allocations.get(id)).
-					setState(newState).
-					build());
-		} else {
-			LOG.log(Level.WARNING, "attempt to modify allocation ''{0}'' ignored, no such allocation available", id);
+			return false;
 		}
 	}
 
-	synchronized void setReason(String id, String reason) {
-		if (this.allocations.containsKey(id)) {
-			this.allocations.put(id,
-					ResourceAllocation.
-					newBuilder(this.allocations.get(id)).
-					setDescription(this.allocations.get(id).getDescription() + ": " + reason).
-					build());
-		} else {
-			LOG.log(Level.WARNING, "attempt to modify allocation ''{0}'' ignored, no such allocation available", id);
+	ResourceAllocation get(String id) {
+		synchronized (this.allocations) {
+			if (this.allocations.containsKey(id)) {
+				ResourceAllocation a = this.allocations.get(id);
+				return a;
+			} else {
+				LOG.log(Level.WARNING, "attempt to query for allocation ''{0}'' ignored, no such allocation available", id);
+			}
+			return null;
 		}
 	}
 
-	synchronized void init(ResourceAllocation allocation) {
-		this.allocations.put(allocation.getId(), allocation);
-		this.notifications.init(allocation.getId());
-	}
-
-	synchronized void schedule(ResourceAllocation allocation) {
-		if (this.allocations.containsKey(allocation.getId())) {
-			setState(allocation.getId(), SCHEDULED);
-			this.notifications.update(allocation.getId());
-		} else {
-			LOG.log(Level.WARNING, "attempt to schedule allocation ''{0}'' ignored, no such allocation available", allocation.getId());
+	ResourceAllocation setState(String id, State newState) {
+		synchronized (this.allocations) {
+			if (this.allocations.containsKey(id)) {
+				return this.allocations.put(id,
+						ResourceAllocation.
+						newBuilder(this.allocations.get(id)).
+						setState(newState).
+						build());
+			} else {
+				LOG.log(Level.WARNING, "attempt to modify allocation ''{0}'' ignored, no such allocation available", id);
+				return null;
+			}
 		}
 	}
 
-	synchronized void reject(ResourceAllocation allocation, String reason) {
-		if (this.allocations.containsKey(allocation.getId())) {
-			setState(allocation.getId(), REJECTED);
-			setReason(allocation.getId(), reason);
-			this.notifications.update(allocation.getId());
-			this.allocations.remove(allocation.getId());
-		} else {
-			LOG.log(Level.WARNING, "attempt to reject allocation ''{0}'' ignored, no such allocation available", allocation.getId());
+	ResourceAllocation setReason(String id, String reason) {
+		synchronized (this.allocations) {
+			if (this.allocations.containsKey(id)) {
+				return this.allocations.put(id,
+						ResourceAllocation.
+						newBuilder(this.allocations.get(id)).
+						setDescription(this.allocations.get(id).getDescription() + ": " + reason).
+						build());
+			} else {
+				LOG.log(Level.WARNING, "attempt to modify allocation ''{0}'' ignored, no such allocation available", id);
+				return null;
+			}
 		}
 	}
 
-	synchronized void update(ResourceAllocation allocation, String reason) {
-		if (this.allocations.containsKey(allocation.getId())) {
+	void init(ResourceAllocation allocation) {
+		synchronized (this.allocations) {
 			this.allocations.put(allocation.getId(), allocation);
-			setReason(allocation.getId(), reason);
-			this.notifications.update(allocation.getId());
-			if (!isAlive(allocation.getId())) {
-				this.allocations.remove(allocation.getId());
-			}
-		} else {
-			LOG.log(Level.WARNING, "attempt to update allocation ''{0}'' ignored, no such allocation available", allocation.getId());
+			this.notifications.init(allocation.getId());
 		}
 	}
 
-	synchronized void release(ResourceAllocation allocation) {
-		if (this.allocations.containsKey(allocation.getId())) {
-			setState(allocation.getId(), RELEASED);
-			this.notifications.update(allocation.getId());
-			this.allocations.remove(allocation.getId());
-		} else {
-			LOG.log(Level.WARNING, "attempt to release allocation ''{0}'' ignored, no such allocation available", allocation.getId());
+	void schedule(ResourceAllocation allocation) {
+		synchronized (this.allocations) {
+			if (isAlive(allocation.getId())) {
+				setState(allocation.getId(), SCHEDULED);
+				this.notifications.update(allocation.getId());
+			} else {
+				LOG.log(Level.WARNING, "attempt to schedule allocation ''{0}'' ignored, no such allocation active", allocation.getId());
+			}
+		}
+	}
+
+	void reject(ResourceAllocation allocation, String reason) {
+		synchronized (this.allocations) {
+			if (isAlive(allocation.getId())) {
+				setState(allocation.getId(), REJECTED);
+				setReason(allocation.getId(), reason);
+				this.notifications.update(allocation.getId());
+				this.allocations.remove(allocation.getId());
+			} else {
+				LOG.log(Level.WARNING, "attempt to reject allocation ''{0}'' ignored, no such allocation active", allocation.getId());
+			}
+		}
+	}
+
+	void update(ResourceAllocation allocation, String reason) {
+		synchronized (this.allocations) {
+			if (isAlive(allocation.getId())) {
+				this.allocations.put(allocation.getId(), allocation);
+				setReason(allocation.getId(), reason);
+				this.notifications.update(allocation.getId());
+				this.allocations.remove(allocation.getId());
+			} else {
+				LOG.log(Level.WARNING, "attempt to update allocation ''{0}'' ignored, no such allocation active", allocation.getId());
+			}
+		}
+	}
+
+	void release(ResourceAllocation allocation) {
+		synchronized (this.allocations) {
+			if (isAlive(allocation.getId())) {
+				System.out.println("from: " + this.allocations.get(allocation.getId()).getState());
+				setState(allocation.getId(), RELEASED);
+				this.notifications.update(allocation.getId());
+				this.allocations.remove(allocation.getId());
+			} else {
+				LOG.log(Level.WARNING, "attempt to release allocation ''{0}'' ignored, no such allocation active", allocation.getId());
+			}
 		}
 	}
 }
