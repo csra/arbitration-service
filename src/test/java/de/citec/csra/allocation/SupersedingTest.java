bb7742ad3f416d44a489261ee3d2261cc4907644
Patrick Holthaus <patrick.holthaus@uni-bielefeld.de>
2016-07-22 10:55:58 +0200

    remove legacy tests

--- src/test/java/de/citec/csra/allocation/SupersedingTest.java
+++ src/test/java/de/citec/csra/allocation/SupersedingTest.java
@@ -17,7 +17,7 @@
 package de.citec.csra.allocation;
 
 import de.citec.csra.allocation.srv.AllocationServer;
-import static org.junit.Assert.assertFalse;
+import java.util.concurrent.TimeoutException;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import org.junit.BeforeClass;
@@ -26,6 +26,10 @@ import rsb.InitializeException;
 import rsb.RSBException;
 import static rst.communicationpatterns.ResourceAllocationType.ResourceAllocation.Policy.MAXIMUM;
 import static rst.communicationpatterns.ResourceAllocationType.ResourceAllocation.Priority.*;
+import static rst.communicationpatterns.ResourceAllocationType.ResourceAllocation.State.ALLOCATED;
+import static rst.communicationpatterns.ResourceAllocationType.ResourceAllocation.State.RELEASED;
+import static rst.communicationpatterns.ResourceAllocationType.ResourceAllocation.State.REQUESTED;
+import static rst.communicationpatterns.ResourceAllocationType.ResourceAllocation.State.SCHEDULED;
 
 /**
  *
@@ -49,110 +53,94 @@ public class SupersedingTest {
 	}
 
 	@Test
-	public void testSchedule() throws InitializeException, RSBException, InterruptedException {
-		SleepingSkill orig = new SleepingSkill("Original", "some-resource", MAXIMUM, NORMAL);
-		orig.activate();
-		orig.schedule(0, 1000);
-		assertTrue("normal termination", orig.await());
-
-		assertTrue("scheduled at least once", orig.getScheduled() >= 1);
-		assertTrue("allocated at least once", orig.getAllocated() >= 1);
-
-		assertTrue("not cancelled", orig.getCancelled() == 0);
-		assertTrue("not rejected", orig.getRejected() == 0);
-		assertTrue("not aborted", orig.getAborted() == 0);
-
+	public void testSchedule() throws InitializeException, RSBException, InterruptedException, TimeoutException {
+		AllocatableResource res = new AllocatableResource("Original", MAXIMUM, NORMAL, "some-resource");
+		assertTrue(res.getState().equals(REQUESTED));
+		res.schedule(2000, 1000);
+		res.await(SCHEDULED, 1000);
+		res.await(ALLOCATED, 3000);
+		res.await(RELEASED, 5000);
 	}
 
 	@Test
-	public void testNonConflict() throws InitializeException, RSBException, InterruptedException {
-		SleepingSkill some = new SleepingSkill("Some", "some-resource", MAXIMUM, NORMAL);
-		SleepingSkill other = new SleepingSkill("Other", "other-resource", MAXIMUM, NORMAL);
-		some.activate();
-		other.activate();
-
-		some.schedule(0, 1000);
-		other.schedule(0, 1000);
-
-		assertTrue("normal termination", some.await());
-		assertTrue("normal termination", other.await());
-
-		assertTrue("scheduled only once", some.getScheduled() == 1);
-		assertTrue("allocated only once", some.getAllocated() == 1);
-
-		assertTrue("not cancelled", some.getCancelled() == 0);
-		assertTrue("not rejected", some.getRejected() == 0);
-		assertTrue("not aborted", some.getAborted() == 0);
-
-		assertTrue("scheduled only once", other.getScheduled() == 1);
-		assertTrue("allocated only once", other.getAllocated() == 1);
-
-		assertTrue("not cancelled", other.getCancelled() == 0);
-		assertTrue("not rejected", other.getRejected() == 0);
-		assertTrue("not aborted", other.getAborted() == 0);
+	public void testNonConflict() throws InitializeException, RSBException, InterruptedException, TimeoutException {
+		AllocatableResource some = new AllocatableResource("Some", MAXIMUM, NORMAL, "some-resource");
+		AllocatableResource other = new AllocatableResource("Other", MAXIMUM, NORMAL, "other-resource");
+		
+		some.schedule(0, 5000);
+		other.schedule(0, 5000);
+		
+		some.await(SCHEDULED, 1000);
+		other.await(SCHEDULED, 1000);
+		
+		some.await(ALLOCATED, 3000);
+		other.await(ALLOCATED, 3000);
+		
+		some.await(RELEASED, 5000);
+		other.await(RELEASED, 5000);
 	}
 
-	@Test
-	public void testShortening() throws InitializeException, RSBException, InterruptedException {
-		SleepingSkill normal = new SleepingSkill("Normal", "some-resource", MAXIMUM, NORMAL);
-		SleepingSkill higher = new SleepingSkill("Higher", "some-resource", MAXIMUM, HIGH);
-		normal.activate();
-		higher.activate();
+//	@Test
+//	public void testShortening() throws InitializeException, RSBException, InterruptedException {
+//		SleepingSkill normal = new SleepingSkill("Normal", "some-resource", MAXIMUM, NORMAL);
+//		SleepingSkill higher = new SleepingSkill("Higher", "some-resource", MAXIMUM, HIGH);
+//		normal.activate();
+//		higher.activate();
+//
+//		normal.schedule(2000, 4000);
+//		long shouldShift = normal.getDuration();
+//
+//		Thread.sleep(500);
+//		
+//		higher.schedule(3000, 2000);
+//		long shouldStay = higher.getDuration();
+//
+//		
+//		assertTrue("normal termination", higher.await());
+//		assertTrue("normal termination", normal.await());
+//
+//		long modified = normal.getDuration();
+//		long stable = higher.getDuration();
+//
+//		assertTrue("normal priority duration shifted", shouldShift > modified);
+//		assertTrue("higher priority duration stable", shouldStay == stable);
+//		
+//		
+//		assertTrue("scheduled only once", higher.getScheduled() == 1);
+//		assertTrue("allocated only once", higher.getAllocated() == 1);
+//
+//		assertTrue("not cancelled", higher.getCancelled() == 0);
+//		assertTrue("not rejected", higher.getRejected() == 0);
+//		assertTrue("not aborted", higher.getAborted() == 0);
+//		
+//		
+//		assertTrue("scheduled twice", normal.getScheduled() == 2);
+//		assertTrue("allocated only once", normal.getAllocated() == 1);
+//
+//		assertTrue("not cancelled", normal.getCancelled() == 0);
+//		assertTrue("not rejected", normal.getRejected() == 0);
+//		assertTrue("not aborted", normal.getAborted() == 0);
+//
+//	}
 
-		normal.schedule(2000, 4000);
-		long shouldShift = normal.getDuration();
-
-		Thread.sleep(500);
-		
-		higher.schedule(3000, 2000);
-		long shouldStay = higher.getDuration();
-
-		
-		assertTrue("normal termination", higher.await());
-		assertTrue("normal termination", normal.await());
-
-		long modified = normal.getDuration();
-		long stable = higher.getDuration();
-
-		assertTrue("normal priority duration shifted", shouldShift > modified);
-		assertTrue("higher priority duration stable", shouldStay == stable);
-		
-		
-		assertTrue("scheduled only once", higher.getScheduled() == 1);
-		assertTrue("allocated only once", higher.getAllocated() == 1);
-
-		assertTrue("not cancelled", higher.getCancelled() == 0);
-		assertTrue("not rejected", higher.getRejected() == 0);
-		assertTrue("not aborted", higher.getAborted() == 0);
-		
-		
-		assertTrue("scheduled twice", normal.getScheduled() == 2);
-		assertTrue("allocated only once", normal.getAllocated() == 1);
-
-		assertTrue("not cancelled", normal.getCancelled() == 0);
-		assertTrue("not rejected", normal.getRejected() == 0);
-		assertTrue("not aborted", normal.getAborted() == 0);
-
-	}
-
-	@Test
-	public void testCancelling() throws InitializeException, RSBException, InterruptedException {
-		SleepingSkill normal = new SleepingSkill("Normal", "some-resource", MAXIMUM, NORMAL);
-		SleepingSkill higher = new SleepingSkill("Higher", "some-resource", MAXIMUM, HIGH);
-		normal.activate();
-		higher.activate();
-
-		normal.schedule(0, 5000);
-		Thread.sleep(500);
-
-		higher.schedule(0, 1000);
-		long shouldStay = higher.getDuration();
-
-		assertTrue("normal termination", higher.await());
-		assertFalse("normal termination", normal.await());
-
-		long stable = higher.getDuration();
-		assertTrue("higher priority duration stable", shouldStay == stable);
-
-	}
+//	@Test
+//	public void testCancelling() throws InitializeException, RSBException, InterruptedException {
+//		SleepingSkill normal = new SleepingSkill("Normal", "some-resource", MAXIMUM, NORMAL);
+//		SleepingSkill higher = new SleepingSkill("Higher", "some-resource", MAXIMUM, HIGH);
+//		normal.activate();
+//		higher.activate();
+//
+//		normal.schedule(0, 5000);
+//		Thread.sleep(500);
+//
+//		higher.schedule(0, 1000);
+//		long shouldStay = higher.getDuration();
+//
+//		assertTrue("normal termination", higher.await());
+//		assertFalse("normal termination", normal.await());
+//
+//		long stable = higher.getDuration();
+//		assertTrue("higher priority duration stable", shouldStay == stable);
+//
+//	}
 }
